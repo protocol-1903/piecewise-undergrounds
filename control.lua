4ed6750283eab190ff90129def62cf5fa7d49e9e
protocol_1903 <fisher.braydon@yahoo.com>
2025-05-23 15:30:53 -0700

    change to storage[unit_number] and attempt at rotation support

--- control.lua
+++ control.lua
@@ -1,13 +1,7 @@
 local ordered_deconstruction = {}
 local cancelled_deconstruction = {}
-local remove_from_storage = {}
 
 local xutil = require "xutil"
-
--- init
-script.on_init(function()
-  storage.under_construction = {}
-end)
 
 -- per tick, might slow down if required
 script.on_event(defines.events.on_tick, function()
@@ -15,7 +9,7 @@ script.on_event(defines.events.on_tick, function()
   cancelled_deconstruction = {}
   local checked = {}
 
-  for index, entities in pairs(storage.under_construction) do
+  for index, entities in pairs(storage) do
 
     -- make sure they aren't marked for deconstruction, and are valid
     if not checked[index] and entities[1].valid and entities[2].valid and not entities[1].to_be_deconstructed() and not entities[2].to_be_deconstructed() then
@@ -47,42 +41,92 @@ script.on_event(defines.events.on_tick, function()
         storage[entities[1].unit_number] = nil
         storage[entities[2].unit_number] = nil
 
+        local fluids = {
+          entities[1].fluidbox[1],
+          entities[2].fluidbox[1]
+        }
+
+        local new_entities = {}
+
+        -- remove fluids before messing around
+        entities[1].fluidbox[1] = nil
+
         -- revert fake undergrounds to normal ones
-        for _, underground in pairs(entities) do
+        for i, underground in pairs(entities) do
           -- make new entity
-          underground.surface.create_entity {
+          local new_entity = underground.surface.create_entity {
             name = xutil.get_type.base(underground),
             position = underground.position,
             direction = underground.direction,
             quality = underground.quality,
             force = underground.force,
             create_build_effect_smoke = false,
-            -- type = underground.type == "underground-belt" and underground.belt_to_ground_type or nil
+            type = xutil.is_belt(entity) and entity.belt_to_ground_type
           }
+
+          new_entities[i] = new_entity
+
           -- remove old entity
           underground.destroy()
         end
+
+        local current_fluid = new_entities[1].fluidbox[1] and {
+          name = new_entities[1].fluidbox[1].name,
+          amount = new_entities[1].fluidbox[1] and new_entities[1].fluidbox.get_fluid_segment_contents(1)[new_entities[1].fluidbox[1].name],
+          temperature = new_entities[1].fluidbox[1].temperature
+        }
+
+        if current_fluid then
+          -- fluid exists, check if it agrees with the old fluids. if not then complex logic
+          -- for loop works great cause it skips nil entities, meaning no extra logic required
+          local found = false
+          for _, fluid in pairs(fluids) do
+            if current_fluid.name == fluid.name then
+              current_fluid.amount = current_fluid.amount + fluid.amount
+            else
+              found = true
+            end
+          end
+          -- some extra logic for another edge case
+          while found do
+            found = false
+            for _, fluid in pairs(fluids) do
+              if current_fluid.name ~= fluid.name and fluid.amount > current_fluid.amount then
+                current_fluid = fluid
+                found = true
+              end
+            end
+          end
+          new_entities[1].fluidbox[1] = current_fluid
+        elseif fluids[1] and fluids[2] then
+          if fluids[1].name == fluids[2].name then
+            -- same fluid, add amounts together and add to fluidbox
+            fluids[1].amount = fluids[1].amount + fluids[2].amount
+            new_entities[1].fluidbox[1] = fluids[1]
+          else
+            -- not the same fluid, copy whichever has more
+            new_entities[1].fluidbox[1] = fluids[fluids[1].amount > fluids[2].amount and 1 or 2]
+          end
+        elseif fluids[1] or fluids[2] then
+          new_entities[1].fluidbox[1] = fluids[1] or fluids[2]
+        end
+
       else -- make sure we only check one of the pair per tick
         checked[entities[1].unit_number] = true
         checked[entities[2].unit_number] = true
       end
     end
   end
-
-  -- remove unneeded references
-  for index, _ in pairs(remove_from_storage) do
-    storage.under_construction[index] = nil
-  end
 end)
 
 local function order_construction(entity, neighbour, player)
-  game.print("order construction")
 
   local distance = xutil.distance(entity, neighbour)
   local dir = xutil.relative_direction(entity, neighbour)
 
   local amount_required = distance - entity.surface.count_entities_filtered {
     name = xutil.get_type.psuedo(entity),
+    quality = entity.quality.name,
     area = { -- this bs because reasons
       {
         entity.position.x <= neighbour.position.x and entity.position.x - 0.01 or neighbour.position.x - 0.01,
@@ -96,8 +140,6 @@ local function order_construction(entity, neighbour, player)
   }
 
   local player_built
-
-  game.print(prototypes.entity[xutil.get_type.psuedo(entity)].items_to_place_this[1].name)
 
   if amount_required > 0 and player and player.get_main_inventory().get_item_count{
     name = prototypes.entity[xutil.get_type.psuedo(entity)].items_to_place_this[1].name,
@@ -141,7 +183,6 @@ local function order_construction(entity, neighbour, player)
         create_build_effect_smoke = false
       }
     elseif #existing_units == 1 then
-      game.print("found an existing unit")
       -- entity exists, cancel deconstruction
       cancelled_deconstruction[existing_units[1].unit_number] = true
       ordered_deconstruction[existing_units[1].unit_number] = nil
@@ -150,16 +191,15 @@ local function order_construction(entity, neighbour, player)
   end
 
   -- add to construction checks
-  storage.under_construction[entity.unit_number] = {entity, neighbour}
-  storage.under_construction[neighbour.unit_number] = {entity, neighbour}
+  storage[entity.unit_number] = {entity, neighbour}
+  storage[neighbour.unit_number] = {entity, neighbour}
 end
 
 local function cancel_construction(entity, player)
-  game.print("cancel construction")
-  local entities = storage.under_construction[entity.unit_number]
+  local entities = storage[entity.unit_number]
   local other_entity = entities[1] == entity and entities[2] or entities[1]
-  storage.under_construction[entities[1].unit_number] = nil
-  storage.under_construction[entities[2].unit_number] = nil
+  storage[entities[1].unit_number] = nil
+  storage[entities[2].unit_number] = nil
 
   -- mark extraneous underground entities for decosntruction/remove by player
   local undergrounds = entities[1].surface.find_entities_filtered {
@@ -221,10 +261,8 @@ local function cancel_construction(entity, player)
   end
 end
 
-local function order_deconstruction(entity, ignore, player, dont_replace)
-  game.print("order deconstruction")
-  dont_replace = dont_replace or false
-  local other_entity = xutil.get_neighbour(entity, ignore)
+local function order_deconstruction(entity, ignore, player, direction)
+  local other_entity = xutil.get_neighbour(entity, ignore, direction)
 
   local distance = xutil.distance(entity, other_entity)
   local dir = xutil.relative_direction(entity, other_entity)
@@ -235,14 +273,12 @@ local function order_deconstruction(entity, ignore, player, dont_replace)
     quality = entity.quality,
     count = distance
   } then -- only insert items if every single one can be inserted
-    game.print("give pipes to player")
     player.get_main_inventory().insert{
       name = xutil.get_type.pipe(entity),
       quality = entity.quality,
       count = distance
     }
   else -- create underground entities and mark for deconstruction
-    game.print("make psuedos")
     for i = 0.5, distance - 0.5 do
       local unit = entity.surface.create_entity {
         name = xutil.get_type.psuedo(entity),
@@ -259,37 +295,82 @@ local function order_deconstruction(entity, ignore, player, dont_replace)
     end
   end
 
+  local new_entities = {}
+
+  local fluid = other_entity.fluidbox[1]
+  local total_fluid = (other_entity.get_fluid(1) and other_entity.get_fluid(1).amount or 0) + ((direction or entity.to_be_deconstructed()) and entity.get_fluid(1) and entity.get_fluid(1).amount or 0)
+
   -- replace existing pipes with incomplete variants
-  for i, underground in pairs{not dont_replace and entity or nil, other_entity} do
-    -- local fluid = entity.getentity.get_fluid[1]
-    -- fluid.amount = entity.get_fluid_count()
+  for i, underground in pairs{(direction or entity.to_be_deconstructed()) and entity or nil, other_entity} do
     -- create new entity
-    local neighbour = underground.surface.create_entity {
+    local new_entity = underground.surface.create_entity {
       name = xutil.get_type.incomplete(underground),
       position = underground.position,
       direction = underground.direction,
       quality = underground.quality,
       force = underground.force,
       create_build_effect_smoke = false
-    }--.insert_fluid(fluid)
+    }
+
     if underground.to_be_deconstructed() then
-      neighbour.order_deconstruction(neighbour.force)
+      new_entity.order_deconstruction(new_entity.force)
     end
+
     -- remove old entity
     underground.destroy()
-    
-    if i == 2 then return neighbour end
+
+    new_entities[i] = new_entity
+  end
+
+  local remaining_fluid = 0
+
+  -- there was some fluid... figure out how much to put where
+  if fluid then
+    for _, entity in pairs(new_entities) do
+      for _, connection in pairs(entity.fluidbox.get_pipe_connections(1)) do
+        if connection.target and connection.target[1] then
+          remaining_fluid = remaining_fluid + connection.target.get_fluid_segment_contents(1)[connection.target[1].name]
+        end
+      end
+    end
+  end
+
+  -- if some fluid not accounted for, put it into the old entities
+  if total_fluid > remaining_fluid then
+    for _, entity in pairs(new_entities) do
+      fluid.amount = (entity.fluidbox[1] and entity.fluidbox.get_fluid_segment_contents(1)[entity.fluidbox[1].name] or 0) + (total_fluid - remaining_fluid) / 2
+      entity.fluidbox[1] = fluid
+    end
+  end
+
+  return new_entities
+end
+
+local function attempt_to_construct(entity, player)
+  local neighbour = xutil.get_neighbour(entity)
+
+  if not neighbour or neighbour.to_be_deconstructed() then return end
+  
+  if xutil.is_type.incomplete(neighbour) then
+    if storage[neighbour.unit_number] then
+      cancel_construction(neighbour)
+    end
+
+    order_construction(entity, neighbour, player)
+
+  elseif xutil.is_type.base(neighbour) then
+    new_entities = order_deconstruction(neighbour, entity, player, neighbour.direction)
+
+    order_construction(entity, new_entities[1], player)
   end
 end
 
 --- @param event EventData.on_built_entity|EventData.on_robot_built_entity|EventData.script_raised_built|EventData.script_raised_revive|EventData.on_space_platform_built_entity|EventData.on_cancelled_deconstruction
 local function on_constructed(event)
-  game.print("on constructed")
   local entity = event.entity
   local player = event.name ~= defines.events.on_cancelled_deconstruction and event.player_index and game.players[event.player_index]
 
-  if entity.type == "entity-ghost" and xutil.is_type.base(entity) then
-    game.print("bad ghost to normal")
+  if xutil.is_type.base(entity) then
 
     if entity.surface.count_entities_filtered{
       name = xutil.get_type.incomplete(entity),
@@ -310,63 +391,41 @@ local function on_constructed(event)
       end
     else
       -- somehow got a normal variant, replace with a base one
-      entity.surface.create_entity {
-        name = "entity-ghost",
-        ghost_name = xutil.get_type.incomplete(entity),
+      local new_entity = entity.surface.create_entity {
+        name = entity.name == "entity-ghost" and "entity-ghost" or xutil.get_type.incomplete(entity),
+        ghost_name = entity.name == "entity-ghost" and xutil.get_type.incomplete(entity),
         position = entity.position,
         direction = entity.direction,
         quality = entity.quality,
         force = entity.force,
-        type = entity.ghost_type == "underground-belt" and entity.belt_to_ground_type or nil
+        type = xutil.is_belt(entity) and entity.belt_to_ground_type,
+        raise_built = true
       }
+
+      local fluid = entity.get_fluid(1)
+
+      -- remove old entity
       entity.destroy()
+
+      if fluid then new_entity.fluidbox[1] = fluid end
     end
   elseif entity.name ~= "entity-ghost" and xutil.is_type.psuedo(entity) and event.name == defines.events.on_cancelled_deconstruction and not cancelled_deconstruction[entity.unit_number] then
     ordered_deconstruction[entity.unit_number] = true
     entity.order_deconstruction(entity.force)
   elseif entity.name ~= "entity-ghost" and xutil.is_type.incomplete(entity) then
-    game.print("is incomplete, attempt to reconnect")
-    -- was previously marked for deconstruction (assumed)
-
-    local neighbour = xutil.get_neighbour(entity)
-    
-    if xutil.is_type.incomplete(neighbour) then
-      -- found a neighbour, either under construction or isolated
-
-      -- check if neighbour is under construction
-      if storage.under_construction[neighbour.unit_number] then
-        game.print("decon then recon")
-        -- deconstruct old pair
-        cancel_construction(neighbour)
-        -- construct new pair
-        order_construction(entity, neighbour)
-      else -- isolated entity, mark both for construction
-        game.print("construct new pair")
-        order_construction(entity, neighbour)
-      end
-
-    elseif xutil.is_type.base(neighbour) then
-      -- placed in between constructed pair, deconstruct them
-      game.print("decon full pair and recon under con pair")
-
-      neighbour = order_deconstruction(neighbour, entity, player)
-
-      order_construction(entity, neighbour, player)
-    end
+    attempt_to_construct(entity, player)
   end
 end
 
 --- @param event EventData.on_player_mined_entity|EventData.on_robot_mined_entity|EventData.script_raised_destroy|EventData.on_space_platform_mined_entity|EventData.on_entity_died
 local function on_deconstructed(event)
-  game.print("on deconstructed")
   local entity = event.entity
   local player = event.name ~= defines.events.on_marked_for_deconstruction and event.player_index and game.players[event.player_index]
 
-  if xutil.is_type.incomplete(entity) and storage.under_construction[entity.unit_number] then
-    game.print("cancel construction for deconstructed entity")
+  if xutil.is_type.incomplete(entity) and storage[entity.unit_number] then
 
     -- get current neighbour and potential new neighbour
-    local entities = storage.under_construction[entity.unit_number]
+    local entities = storage[entity.unit_number]
     local other_entity = entities[1] == entity and entities[2] or entities[1]
     local new_neighbour = xutil.get_neighbour(other_entity, entity)
 
@@ -374,27 +433,24 @@ local function on_deconstructed(event)
     cancel_construction(entity, player)
 
     -- remove old entities from storage
-    storage.under_construction[entity.unit_number] = nil
-    storage.under_construction[other_entity.unit_number] = nil
+    storage[entity.unit_number] = nil
+    storage[other_entity.unit_number] = nil
 
     -- if new neighbour exists, attempt to construct those
-    if new_neighbour then
+    if new_neighbour and not new_neighbour.to_be_deconstructed() then
       order_construction(other_entity, new_neighbour, player)
     end
-
-    game.print("fidoshaiofhiodhsao")
 
   elseif xutil.is_type.base(entity) then
     -- completed pair, so 'deconstruct' both and delete this one
     local other_entity = xutil.get_neighbour(entity)
     local new_neighbour = xutil.get_neighbour(other_entity, entity)
-    other_entity = order_deconstruction(entity, nil, player, true)
+    other_entity = order_deconstruction(entity, nil, player)[2]
 
     if new_neighbour then
       order_construction(other_entity, new_neighbour, player)
     end
   elseif entity.name ~= "entity-ghost" and xutil.is_type.psuedo(entity) and event.name == defines.events.on_marked_for_deconstruction and not ordered_deconstruction[entity.unit_number] then
-    game.print("cancelled deconstruction of psuedos", {skip = defines.print_skip.never})
     cancelled_deconstruction[entity.unit_number] = true
     entity.cancel_deconstruction(entity.force)
   end
@@ -462,4 +518,35 @@ script.on_event(defines.events.on_player_setup_blueprint, function (event)
   blueprint.set_blueprint_entities(entities)
 end)
 
--- TODO undo/redo: whenever i undo/redo a mark for deconstruction, or entity removal, or entity place...
+-- TODO undo/redo
+-- TODO rotations
+
+script.on_event(defines.events.on_player_rotated_entity, function (event)
+  local entity = event.entity
+  local player = game.players[event.player_index]
+  player = player.controller_type ~= defines.controllers.remote and player
+
+  -- we dont care if its a ghost
+  if entity.name == "entity-ghost" then return end
+
+  if xutil.is_type.base(entity) then
+
+    -- is complete, deconstruct
+    local new_entities = order_deconstruction(entity, nil, player, event.previous_direction)
+
+    -- attempt to construct this entity
+    attempt_to_construct(new_entities[1], player)
+
+    -- check if the old neighbour has a new one
+    attempt_to_construct(new_entities[2], player)
+  
+  elseif xutil.is_type.incomplete(entity) then
+    -- is incomplete attempt to construct
+    if storage[entity.unit_number] then
+      cancel_construction(entity, player)
+    end
+
+    attempt_to_construct(entity, player)
+
+  end
+end)
